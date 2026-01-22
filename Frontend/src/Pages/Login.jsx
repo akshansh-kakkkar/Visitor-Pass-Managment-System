29aab5843244cf0ed9ad89e24af3aa5ce8e7fc20
Akshasnh Kakkar <akshanshkakkar92@gmail.com>
2026-01-22 09:52:54 +0530

    final

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import api from '../api/api'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import BgGlow from '../Components/BgGlow';
 import eyeIcon from '../../assets/eye.svg'
 import eyeSlashIcon from '../../assets/eye-slash.svg'
@@ -44,10 +44,10 @@ const Login = () => {
           <div className='font-bold justify-center p-5 rounded-2xl px-8 text-4xl  bg-gradient-to-r from-purple-500 to-indigo-500 hover:shadow-[0_2px_45px_rgba(139,92,246,0.8)] transition-all'>
             <h1>P</h1>
           </div>
-          <h1 className='text-center text-2xl font-bold font-semiboldbg-gradient-to-r from-purple-400 to-indigo-400 bg-clip-text text-transparen'>Welcome Back</h1>
+          <h1 className='text-center text-2xl font-bold bg-gradient-to-r from-purple-400 to-indigo-400 bg-clip-text text-transparent'>Welcome Back</h1>
           <p className='text-sm text-gray-400'>Enter your credentials to access PASSIFY</p>
 
-          <label htmlFor="email"className='flex -translate-x-28  sm:-translate-x-37 translate-y-4'>Email</label>
+          <label htmlFor="email" className='flex -translate-x-28  sm:-translate-x-37 translate-y-4'>Email</label>
           <div className='relative w-full'>
             <input className='w-full px-4 py-3 rounded-xl bg-white/5 border border-white/10 text-white placeholder-white/40 outline-none focus:border-purple-500/60 focus:shadow-[0_0_0_1px_rgba(139,92,246,0.4)] transition'
               type="email"
@@ -76,11 +76,12 @@ const Login = () => {
           </div>
           <button type='submit' className='mt-4 w-full py-3 rounded-xl bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-medium shadow-[0_12px_30px_rgba(139,92,246,0.6)] hover:scale-[1.03] hover:shadow-[0_18px_45px_rgba(139,92,246,0.8)] transition-all'>LOGIN</button>
           <div className='w-80 h-[0.3px] mt-7 bg-gray-800 rounded-full'></div>
-          <p className='text-sm text-gray-400 font-bold'>Not an employee?  <span className='text-purple-600 font-semibold hover:underline'><a href="/visitor-register">click here</a></span> </p>
+          <p className='text-sm text-gray-400 font-bold'>Not an employee?  <span className='text-purple-600 font-semibold hover:underline'><Link to="/visitor-register">click here</Link></span> </p>
           {error && <p className="text-red-400 text-sm text-center mt-3 relative z-10">{error}</p>}
         </form>
       </div>
     </>
+
   )
 }
 
