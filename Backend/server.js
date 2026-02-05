2ee4583f6b47d0cf5680c5956109c3b0c29174e9
Akshasnh Kakkar <akshanshkakkar92@gmail.com>
2026-02-05 14:16:17 +0530

    push

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,23 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import AuthRoute from './Routes/AuthRoute.js'
-import cors from "cors"
-import ProtectedRoutes from './Routes/ProtectedRoutes.js'
-import AdminRoutes from './Routes/AdminRoutes.js'
-import VisitorRoutes from './Routes/VisitorRoutes.js'
-import AppointmentRoutes from './Routes/AppointmentRoutes.js'
-import PassRoutes from './Routes/PassRoutes.js'
-import CheckLog from './Routes/CheckLogRoutes.js'
-import ViewPass from './Routes/ViewPassRoutes.js'
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import AuthRoute from './Routes/AuthRoute.js';
+import cors from "cors";
+import ProtectedRoutes from './Routes/ProtectedRoutes.js';
+import AdminRoutes from './Routes/AdminRoutes.js';
+import VisitorRoutes from './Routes/VisitorRoutes.js';
+import AppointmentRoutes from './Routes/AppointmentRoutes.js';
+import PassRoutes from './Routes/PassRoutes.js';
+import CheckLog from './Routes/CheckLogRoutes.js';
+import ViewPass from './Routes/ViewPassRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config()
 const app = express()
 const Port = process.env.PORT || 3000
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 app.use(cors());
 app.use(express.json());
@@ -23,14 +28,14 @@ app.use('/api/visitor', VisitorRoutes);
 app.use('/api/visitor', AppointmentRoutes);
 app.use('/api/visitor', PassRoutes);
 app.use('/api/security', CheckLog);
-app.use('/visitor', ViewPass)
-app.use('/passes', express.static('passes'));
+app.use('/visitor', ViewPass);
+app.use("/passes", express.static(path.join(dirname, "passes")));
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('mongodb database is connected');
 }).catch((err) => {
     console.log(err);
 })
-
 
 app.listen(Port, () => {
     console.log(`app is listening at http://localhost:${Port}`);
